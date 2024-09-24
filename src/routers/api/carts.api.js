494a60e573012412e6ef2604a34ad6e083035e3c
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-09-23 20:42:03 -0500

    Ultimos cambios

--- src/routers/api/carts.api.js
+++ src/routers/api/carts.api.js
@@ -1,1 +1,17 @@
+import { Router } from 'express';
+import {
+    getCartByUserId,
+    addProductToCart,
+    removeProductFromCart,
 
+
+} from "../../../src/controllers/carts.controllers.js"
+
+const cartsApiRouter = Router();
+
+cartsApiRouter.get("/", getCartByUserId);
+cartsApiRouter.get("/:pid", addProductToCart);
+cartsApiRouter.delete("/:pid", removeProductFromCart);
+
+
+export default cartsApiRouter;
