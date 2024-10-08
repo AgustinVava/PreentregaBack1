0f4fc0223faa61847137da4000c92ffa89b8d55f
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-10-07 20:51:31 -0500

    Agregando producto final

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { engine } from "express-handlebars";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
 import router from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
@@ -12,7 +14,13 @@ try {
   const server = express();
   const port = 8080;
   const ready = () => console.log("server ready on port " + port);
-  server.listen(port, ready);
+  const httpServer = createServer(server);
+  const tcpServer = new Server(httpServer);
+  tcpServer.on("connection", socket => {
+    console.log("Socket connected is:" + socket.id);
+
+  })
+  httpServer.listen(port, ready);
 
   server.use(morgan("dev"));
   server.use(express.urlencoded({ extended: true }));
