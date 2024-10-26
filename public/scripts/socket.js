dd64355f3f7854806d17694e9148161a94a4d37d
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-10-25 22:19:34 -0500

    Ultimas actualizaciones de Socket, ya esta corriendo y funcionando + mejoras visuales del navbar, preview de los productos, pagina de todos los productos + el formulario de registro

--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -3,12 +3,14 @@ const socket = io();
 document.querySelector("#register").addEventListener("click", ()=>{
     const name = document.querySelector("#name").value
     const email = document.querySelector("#email").value
+    const photo = document.querySelector("#photo").value
     const password = document.querySelector("#password").value
-    const usersData = {name, email, password}
+    const usersData = {name, email, photo, password}
 
     socket.emit("new user", usersData)
 })
 
-socket.on("update users", id=>{
-    document.querySelector("#update").innerHTML = id
+socket.on("update users", data=>{
+    data = data.map(each => `<div>${each.email}</div>`).join("")
+    document.querySelector("#update").innerHTML = data
 })
--- src/routers/index.socket.js
+++ src/routers/index.socket.js
@@ -1,11 +1,14 @@
 import usersManager from "../data/users.manager.js";
 
-const socketCallBack = (socket) =>{
+const socketCallBack = async (socket) =>{
     console.log("socket connected id: " + socket.id)
 socket.on("new user", async data =>{
 const id = await usersManager.create(data)
-socket.emit("update users", id)
-}) 
+socket.emit("update users", allUsers)
+})
+const allUsers = await usersManager.readAll()
+socket.emit("update users", allUsers)
+
 }
 
 export default socketCallBack;
